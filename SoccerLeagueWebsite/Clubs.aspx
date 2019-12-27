e69115891bdf98ac83b71d090f89dce1c2608f26
gr8sid <gr8sid7981@gmail.com>
2019-12-27 06:45:22 -0500

    Club List display page added - using data binding by ClubID.

---
+++ SoccerLeagueWebsite/Clubs.aspx
@@ -0,0 +1,64 @@
+﻿<%--Name: Sidharth Choudhary    Student#- 300990285--%>
+<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" MaintainScrollPositionOnPostback="true" AutoEventWireup="true" CodeFile="Clubs.aspx.cs" Inherits="Clubs" %>
+<%@ Register Src="~/AddDisplayClubs.ascx" TagName="AddClub" TagPrefix="AC" %>
+
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="FooterContentPlaceHolder" Runat="Server">
+</asp:Content>
+
+<asp:Content ID="Content3" ContentPlaceHolderID="MainContentPlaceHolder" Runat="Server">
+
+<h1 style="text-align:center;">
+    **BE A PART OF OUR CLUBS**
+</h1>
+    
+   
+        
+   
+    <form runat="server">
+
+        <div id="Login" style="font-size:30px; font-family:'Comic Sans MS';margin-left:13%;margin-top:10%; z-index: 1; left: -43px; top: -126px; position: absolute; height: 23px; width: 891px;">
+      
+      <asp:LoginView ID="LoginView1" runat="server">
+            <AnonymousTemplate>
+                 <asp:LoginStatus ID="LoginStatus1" runat="server" Font-Underline="false" />
+                Restricted Page
+            </AnonymousTemplate>
+
+            <LoggedInTemplate>
+                &nbsp<asp:LoginName ID="LoginName1" runat="server" FormatString ="Hello, {0}" LogoutPageUrl="~/Login.aspx"/> 
+                <asp:LoginStatus ID="LoginStatus2" runat="server" Font-Underline="false"/>
+            </LoggedInTemplate>
+        </asp:LoginView>
+    </div>
+
+   <table style="align-content:center; width:50%; margin-left:25%; border:groove; border-width:3px"> 
+       <tr>
+           <th><b>CLUB NAMES</b><hr /></th>
+           
+       </tr>
+       
+      <tr>
+          <td>
+          <asp:DataList ID="ClubDetailsList" runat="server" OnItemCommand="ClubDetailsList_ItemCommand">
+              <ItemTemplate>
+                  <%# Eval("CLUBID") %>. 
+             <asp:LinkButton ID="DetailsButton" runat="server" CommandName="MoreDetailsButton" CommandArgument = '<%# Eval("CLUBID") %>' Font-Underline="false" Font-Size="X-Large" ForeColor="#006600" ><%# Eval("CLUBNAME") %></asp:LinkButton>
+            <asp:Literal ID="extraDetailsLiteral" runat="server" EnableViewState="false"></asp:Literal>
+              </ItemTemplate>
+          </asp:DataList> 
+          </td>
+      </tr>
+   </table>
+ 
+      </form>
+</asp:Content>
+
+<asp:Content ID="Content4" ContentPlaceHolderID="SideContentPlaceHolder" Runat="Server">
+    <h1 style="text-align:center;">
+    **Pages Under Construction**
+</h1>
+</asp:Content>
+<%--Name: Sidharth Choudhary    Student#- 300990285--%>
---
+++ SoccerLeagueWebsite/Clubs.aspx.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Collections;
+using System.Configuration;
+using System.Data.SqlClient;
+//Name: Sidharth Choudhary    Student#- 300990285
+public partial class Clubs : System.Web.UI.Page
+{
+    string connectionString = ConfigurationManager.ConnectionStrings["SqlConn"].ConnectionString;
+    SqlConnection conn = new SqlConnection();
+    int clubIdSelected;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if(!IsPostBack)
+        {
+            databind();
+        }
+        
+    }
+
+    protected void Page_PreInit(object sender, EventArgs e)
+    {
+        if (Session["theme"] != null)
+        {
+            Page.Theme = Session["theme"].ToString();
+        }
+    }
+
+    private void databind()
+    {
+        conn.ConnectionString = connectionString;
+        SqlCommand comm = new SqlCommand("select clubid,clubname from clubs", conn);
+        SqlDataReader reader;
+
+        try
+        {
+            conn.Open();
+            reader = comm.ExecuteReader();
+            ClubDetailsList.DataSource = reader;
+            ClubDetailsList.DataBind();
+        }
+
+        finally
+        {
+            conn.Close();
+        }
+         
+
+    }
+    
+    protected void ClubDetailsList_ItemCommand(object source, DataListCommandEventArgs e)
+    {
+        if(e.CommandName== "MoreDetailsButton")
+        {
+           // Literal li;
+            //li = (Literal)e.Item.FindControl("extraDetailsLiteral");
+           clubIdSelected = Convert.ToInt32(e.CommandArgument);
+            Session["clubIdSelected"] = clubIdSelected;
+            //li.Text = "Club Id: <strong>" + Session["clubIdSelected"] + "</strong><br />";
+            Response.Redirect("ClubDetail.aspx");
+        }
+    }
+}
+//Name: Sidharth Choudhary    Student#- 300990285
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+     * while (reader.Read())
+            {
+                DataList dl = new DataList(reader["clubname"}.ToString());
+                for(int i = 0; i < reader.FieldCount; i++)
+                {
+                    ClubDetailsList.a
+                }
+            }
+     * 
+     * 
+     * public class club
+    {
+        private string clubName;
+
+        public string ClubName
+        {
+            get { return clubName; }
+            set { clubName = value; }
+        }
+        private string clubCity;
+
+        public string ClubCity
+        {
+            get { return clubCity; }
+            set { clubCity = value; }
+        }
+
+    }*/
+
+
+
+
+
+
