c68649ee31165cac2889154486343fba4630b4c7
gr8sid <gr8sid7981@gmail.com>
2019-12-29 10:19:40 -0500

    Configured web.config and added ClubDetails page

---
+++ SoccerLeagueWebsite/ClubDetail.aspx
@@ -0,0 +1,199 @@
+﻿<%--Name: Sidharth Choudhary    Student#- 300990285--%>
+<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" MaintainScrollPositionOnPostback="true" AutoEventWireup="true" CodeFile="ClubDetail.aspx.cs" Inherits="ClubDetail" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+    <link rel="stylesheet" href="gridStyle.css" />
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="SideContentPlaceHolder" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="MainContentPlaceHolder" Runat="Server">
+
+    <h1 style="text-align:center;">
+    **CLUB DETAILS **
+    </h1>
+    <form runat="server">
+        <div id="Login" style="font-size:30px; font-family:'Comic Sans MS';margin-left:13%;margin-top:10%; z-index: 1; left: -43px; top: -126px; position: absolute; height: 23px; width: 891px;">
+      
+      <asp:LoginView ID="LoginView1" runat="server">
+            <AnonymousTemplate>
+                 <asp:LoginStatus ID="LoginStatus1" runat="server" Font-Underline="false" />
+            </AnonymousTemplate>
+
+            <LoggedInTemplate>
+                &nbsp<asp:LoginName ID="LoginName1" runat="server" FormatString ="Hello, {0}" LogoutPageUrl="~/Login.aspx"/> 
+                <asp:LoginStatus ID="LoginStatus2" runat="server" Font-Underline="false"/>
+            </LoggedInTemplate>
+        </asp:LoginView>
+    </div>
+        <div style="margin-left:8%" >
+            <asp:linkbutton id="GoBackLink" runat="server" onclick="GoBackLink_Click" visible="False" forecolor="PaleTurquoise">CLUB DELETED SUCCESSFULLY !! Go Back to CLUBS List</asp:linkbutton>
+        </div>
+
+    <div style="margin-left:20%">
+       
+    <asp:DetailsView id="ClubDetailsView" runat="server" cellpadding="4" forecolor="#333333" autogeneraterows="False" onitemdeleting="ClubDetailsView_ItemDeleting">
+        <AlternatingRowStyle BackColor="White" />
+        <CommandRowStyle BackColor="#D1DDF1" Font-Bold="True" />
+        <EditRowStyle BackColor="#2461BF" />
+        <FieldHeaderStyle BackColor="#DEE8F5" Font-Bold="True" />
+        <Fields>
+            <asp:BoundField DataField="ClubId" HeaderText="ID" />
+            <asp:BoundField DataField="ClubName" HeaderText="CLUB NAME" />
+            <asp:BoundField DataField="ClubCity" HeaderText="CITY" />
+            <asp:BoundField DataField="Address" HeaderText="ADDRESS" />
+            <asp:BoundField DataField="registrationNumber" HeaderText="REGISTRATION NUMBER" />
+           
+            <%--<asp:CommandField HeaderText="Delete Club" ShowDeleteButton="True" ShowHeader="True" />--%>
+           
+            <asp:CommandField  />
+            <asp:TemplateField ShowHeader="true" HeaderText="DELETE THE CLUB">
+                <ItemTemplate>
+                    <asp:LinkButton ID="DeleteLinkButton"  Font-Bold="true" Font-Underline="false" runat="server" CausesValidation="False" CommandName="Delete" Text="Delete Club" OnClientClick="return confirm('Are you sure you want to delete Club');" BorderWidth="2px"></asp:LinkButton>
+                </ItemTemplate>
+            </asp:TemplateField>
+           
+        </Fields>
+        <FooterStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+        <HeaderStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+        <PagerStyle BackColor="#2461BF" ForeColor="White" HorizontalAlign="Center" />
+        <RowStyle BackColor="#EFF3FB" />
+    </asp:DetailsView>
+        </div><br />
+
+        <asp:Literal ID="NoPlayerLiteral" runat="server" EnableViewState="false"></asp:Literal>
+
+       <div style="margin-left:5%"> 
+        <asp:GridView ID="PlayerGrid" runat="server" AutoGenerateColumns="False" 
+            OnSelectedIndexChanged="PlayerGrid_SelectedIndexChanged" DataKeyNames="PlayerId">
+
+            <Columns>
+                <asp:TemplateField>
+                    <ItemTemplate>
+                        <asp:Image ID="ProfileImg"  AlternateText="No Image" runat="server" Style="position: relative; height:60px; width: 50px" ImageUrl='<%# Eval("ProfileImage") %>' />
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:BoundField DataField="playername" HeaderText="PLAYER NAME" />
+                <asp:BoundField DataField="jerseynumber" HeaderText="JERSEY NUMBER" />
+                <asp:BoundField DataField="DATEOFBIRTH" HeaderText="DATE OF BIRTH" />
+                <asp:ButtonField Text="Update Details" CommandName="Select" ControlStyle-Font-Underline="false" >
+<ControlStyle Font-Underline="False"></ControlStyle>
+                </asp:ButtonField>
+               <%-- <asp:ButtonField Text="Update Profile Picture" CommandName="Select" ControlStyle-Font-Underline="false" />--%>
+                <%--<ItemTemplate>  
+                      <asp:Image runat="server" ID="imgProfile" ImageUrl='<%# Eval("ImagePath") %>' Width="400px" Height="150px" />  
+                </ItemTemplate>--%>
+              
+            </Columns>
+            <HeaderStyle CssClass="gridHeader" />
+            <RowStyle CssClass="gridRow" />
+            <SelectedRowStyle CssClass="gridSelectedRow" />
+        </asp:GridView>
+        <br />
+    <asp:Label ID="detailsLabel" runat="server" />
+           </div>
+        
+        <div style="margin-left:30%">
+          <asp:DetailsView ID="PLayerDetailView" runat="server" Height="50px" Width="15px" 
+            AutoGenerateRows="False" OnItemUpdating="PLayerDetailView_ItemUpdating" OnItemCommand="PLayerDetailView_ItemCommand" OnModeChanging="PLayerDetailView_ModeChanging" OnItemDeleting="PLayerDetailView_ItemDeleting1">
+           <Fields>
+               <asp:TemplateField ShowHeader="true" HeaderText="DELETE THE Player">
+                <ItemTemplate>
+                    <asp:LinkButton ID="DeleteLinkButton"  Font-Bold="true" Font-Underline="false" runat="server" CausesValidation="False" CommandName="Delete" Text="Delete Player" OnClientClick="return confirm('Are you sure you want to Remove the Player');" BorderWidth="2px"></asp:LinkButton>
+                </ItemTemplate>
+            </asp:TemplateField>
+               <asp:TemplateField HeaderText="PlayerName">
+        <EditItemTemplate>
+          <asp:TextBox ID="editPlayerNameTextBox" runat="server" Text='<%# Bind("PlayerName") %>'></asp:TextBox>
+        </EditItemTemplate>
+        <InsertItemTemplate>
+          <asp:TextBox ID="insertPlayerNameTextBox" runat="server" Text='<%# Bind("PlayerName") %>'></asp:TextBox>
+        </InsertItemTemplate>
+        <ItemTemplate>
+          <asp:Label ID="playerNameLabel" runat="server" Text='<%# Bind("PlayerName") %>'></asp:Label>
+        </ItemTemplate>
+      </asp:TemplateField>
+
+                 <asp:TemplateField HeaderText="JerseyNumber">
+        <EditItemTemplate>
+          <asp:TextBox ID="editJerseyNumberTextBox" runat="server" Text='<%# Bind("JerseyNumber") %>'></asp:TextBox>
+                <asp:RangeValidator ID="RangeValidator1" runat="server" ErrorMessage="*Enter a number between 0 to 99" ControlToValidate="editJerseyNumberTextBox" Display="Dynamic" ForeColor="#CC0000" MaximumValue="99" MinimumValue="0" Type="Integer"></asp:RangeValidator>
+           <asp:RequiredFieldValidator ID="RequiredFieldValidator4" runat="server" ControlToValidate="editJerseyNumberTextBox" Display="Dynamic" ErrorMessage="*Please Enter Jersey Number" ForeColor="Red"></asp:RequiredFieldValidator>
+        
+        </EditItemTemplate>
+        <InsertItemTemplate>
+          <asp:TextBox ID="insertJerseyNumberTextBox" runat="server" Text='<%# Bind("JerseyNumber") %>'></asp:TextBox>
+        </InsertItemTemplate>
+        <ItemTemplate>
+          <asp:Label ID="jerseyNumberLabel" runat="server" Text='<%# Bind("JerseyNumber") %>'></asp:Label>
+        </ItemTemplate>
+        </asp:TemplateField>
+
+                    <asp:TemplateField HeaderText="DateOfBirth">
+        <EditItemTemplate>
+          <asp:TextBox ID="editDateOfBirthTextBox" runat="server" Text='<%# Bind("DateOfBirth") %>'></asp:TextBox>
+           </EditItemTemplate>
+        <InsertItemTemplate>
+          <asp:TextBox ID="insertDateOfBirthTextBox" runat="server" Text='<%# Bind("DateOfBirth") %>'></asp:TextBox>
+        </InsertItemTemplate>
+        <ItemTemplate>
+          <asp:Label ID="dateOfBirthLabel" runat="server" Text='<%# Bind("DateOfBirth") %>'></asp:Label>
+        </ItemTemplate>
+      </asp:TemplateField>
+
+<%--                <asp:BoundField DataField="playername" HeaderText="Player Name" />
+                <asp:BoundField DataField="dateofbirth" HeaderText="Date of Birth" />
+                <asp:BoundField DataField="jerseynumber" HeaderText="Jersey Number" />--%>
+                <asp:CommandField ShowEditButton="True" ShowDeleteButton="True"  ShowHeader="false" />
+               
+            </Fields>
+
+             <HeaderTemplate>
+                    <%#Eval("playername")%>
+            </HeaderTemplate>
+
+            <HeaderStyle CssClass="gridHeader" />
+            <RowStyle CssClass="gridRow" />
+        </asp:DetailsView>
+</div>
+        <div style="margin-left:30%">
+        <asp:DetailsView ID="ProfieDetailsView" runat="server" Height="50px" Width="125px"
+            AutoGenerateRows="False" OnItemUpdating="ProfileDetailView_ItemUpdating" OnItemCommand="ProfileDetailView_ItemCommand" OnModeChanging="ProfileDetailView_ModeChanging" OnPageIndexChanging="ProfieDetailsView_PageIndexChanging">
+             <Fields>
+
+               <asp:TemplateField ShowHeader="false">
+        <EditItemTemplate>
+            <asp:FileUpload ID="profileFileUpload" runat="server" BackColor="wHITE" BorderColor="Gray" BorderWidth="3px" />
+            <asp:Label ID="selectImageLabel" runat="server"  Text="Label" style ="color:Red"  Visible="false"></asp:Label>
+        </EditItemTemplate>
+        <InsertItemTemplate>
+          <asp:TextBox ID="insertProfileImageTextBox" runat="server" Text='<%# Bind("ProfileImage") %>'></asp:TextBox>
+        </InsertItemTemplate>
+        <ItemTemplate>
+            <asp:Image runat="server" ID="imgProfile" ImageUrl='<%# Eval("ProfileImage") %>' Width="200px" AlternateText="NO PROFILE PICTURE ADDED FOR THIS PLAYER" /> 
+            <asp:Literal runat="server" id="selectImageLiteral"></asp:Literal>
+        </ItemTemplate>  
+                 </asp:TemplateField>
+                
+                 <asp:CommandField ShowEditButton="True" ShowHeader="false" />
+               </Fields>
+            <HeaderTemplate>
+                    <%#Eval("playername")%> - <strong>PROFILE IMAGE</strong>
+            </HeaderTemplate>
+
+            <HeaderStyle CssClass="gridHeader" />
+            <RowStyle CssClass="gridRow" />
+
+        </asp:DetailsView>
+        </div>
+
+
+
+
+
+    </form>
+         
+</asp:Content>
+<asp:Content ID="Content4" ContentPlaceHolderID="FooterContentPlaceHolder" Runat="Server">
+</asp:Content>
+
+<%--Name: Sidharth Choudhary    Student#- 300990285--%>
---
+++ SoccerLeagueWebsite/ClubDetail.aspx.cs
@@ -0,0 +1,382 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+using System.IO;
+using System.Web.Management;
+//Name: Sidharth Choudhary    Student#- 300990285
+public partial class ClubDetail : System.Web.UI.Page
+{
+    string connectionString = ConfigurationManager.ConnectionStrings["SqlConn"].ConnectionString;
+    SqlConnection conn = new SqlConnection();
+    private static int flag = 0;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+         if (!IsPostBack && flag == 1)
+        {
+            GoBackLink.Visible = true;
+            ClubDetailsView.Visible = false;
+            PlayerGrid.Visible = false;
+            PLayerDetailView.Visible = false;
+            flag = 0;
+        }
+
+        else if (flag == 2)
+        {
+            // Display Alert after a player is deleted
+            ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "alertMessage", "alert('Player Deleted Successfully')", true);
+            databind();
+            flag = 0;
+        }
+        else if (!IsPostBack)
+        {
+            databind();
+        }
+        
+
+    }
+
+    private void databind()
+    {
+        int clubNum = Convert.ToInt32(Session["clubIdSelected"]);
+
+        conn.ConnectionString = connectionString;
+        SqlCommand comm = new SqlCommand("select * from clubs where clubid =  @id", conn);
+        comm.Parameters.AddWithValue("@id", clubNum);
+        SqlDataReader reader, playerReader;
+
+        SqlCommand comPlayer = new SqlCommand("select * from players where clubid = @id", conn);
+        comPlayer.Parameters.AddWithValue("@id", clubNum);
+
+
+        try
+        {
+            conn.Open();
+            reader = comm.ExecuteReader();
+            ClubDetailsView.DataSource = reader;
+            ClubDetailsView.DataBind();
+            reader.Close();
+
+            playerReader = comPlayer.ExecuteReader();
+
+            if (!playerReader.HasRows)
+            {
+                NoPlayerLiteral.Text = "NO PLAYER ADDED IN THIS CLUB";
+            }
+            else
+            {
+                PlayerGrid.DataSource = playerReader;
+                PlayerGrid.DataKeyNames = new string[] { "playerId" };
+                PlayerGrid.DataBind();
+                playerReader.Close();
+            }
+
+        }
+
+        finally
+        {
+            conn.Close();
+        }
+
+    }
+
+
+ 
+
+    protected void ClubDetailsView_ItemDeleting(object sender, DetailsViewDeleteEventArgs e)
+    {
+
+        int clubNum = Convert.ToInt32(Session["clubIdSelected"]);
+        conn.ConnectionString = connectionString;
+        SqlCommand comDeletePlayer = new SqlCommand("delete from players where exists ( select * from players where clubid = @id) and clubid = @id ", conn);
+        comDeletePlayer.Parameters.AddWithValue("@id", clubNum);
+        SqlCommand comDeleteClub = new SqlCommand("delete from clubs where clubid = @id", conn);
+        comDeleteClub.Parameters.AddWithValue("@id", clubNum);
+        try
+        {
+            conn.Open();
+            comDeletePlayer.ExecuteNonQuery();
+            comDeleteClub.ExecuteNonQuery();
+            flag = 1;
+            Response.Redirect("ClubDetail.aspx");
+        }
+        catch (Exception ex)
+        {
+            throw (ex);
+        }
+        finally
+        {
+            comDeleteClub.Dispose();
+            comDeletePlayer.Dispose();
+            conn.Close();
+        }
+    }
+
+    protected void GoBackLink_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("Clubs.aspx");
+    }
+
+
+
+    protected void PlayerGrid_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        //int selectedIndexRow;
+        //selectedIndexRow = PlayerGrid.SelectedIndex;
+        //GridViewRow row = PlayerGrid.Rows[selectedIndexRow];
+        //string name = row.Cells[0].Text;
+        //detailsLabel.Text = "You Selected : " + name + ".";
+        //detailsLabel.Visible = true;
+        //detailsLabel.Text = "byee";
+        bindDetail();
+        bindProfileDetail();
+    }
+
+    protected void bindProfileDetail()
+    {
+        int selectedIndexRow;
+        selectedIndexRow = PlayerGrid.SelectedIndex;
+        int playerId = (int)PlayerGrid.DataKeys[selectedIndexRow].Value;
+        conn.ConnectionString = connectionString;
+        SqlCommand comm = new SqlCommand("select playerid,PLAYERNAME,profileimage from players where playerid = @playerid", conn);
+        //comm.Parameters.AddWithValue("@id", playerId);
+        comm.Parameters.Add("playerid", SqlDbType.Int);
+        comm.Parameters["playerid"].Value = playerId;
+        flag = 0;
+        SqlDataReader reader;
+        try
+        {
+            conn.Open();
+            reader = comm.ExecuteReader();
+           
+            ProfieDetailsView.DataSource = reader;
+            ProfieDetailsView.DataKeyNames = new string[] { "playerid" };
+            ProfieDetailsView.DataBind();
+            reader.Close();
+        }
+
+        finally
+        {
+            conn.Close();
+        }
+
+    }
+
+    protected void bindDetail()
+    {
+        int selectedIndexRow;
+        selectedIndexRow = PlayerGrid.SelectedIndex;
+        int playerId = (int)PlayerGrid.DataKeys[selectedIndexRow].Value;
+        //detailsLabel.Text = playerId.ToString();
+        conn.ConnectionString = connectionString;
+        SqlCommand comm = new SqlCommand("select playerid,playername,jerseynumber,dateofbirth from players where playerid = @playerid", conn);
+        //comm.Parameters.AddWithValue("@id", playerId);
+        comm.Parameters.Add("playerid", SqlDbType.Int);
+        comm.Parameters["playerid"].Value = playerId;
+        flag = 0;
+        SqlDataReader reader;
+        try
+        {
+            conn.Open();
+            reader = comm.ExecuteReader();
+            PLayerDetailView.DataSource = reader;
+            PLayerDetailView.DataKeyNames = new string[] { "playerid" };
+            PLayerDetailView.DataBind();
+            reader.Close();
+        }
+
+        finally
+        {
+            conn.Close();
+        }
+    }
+    protected void PLayerDetailView_ItemCommand(object sender, DetailsViewCommandEventArgs e)
+    {
+
+    }
+
+    protected void PLayerDetailView_ItemUpdating(object sender, DetailsViewUpdateEventArgs e)
+    {
+        int playerid = (int)PLayerDetailView.DataKey.Value;
+        TextBox newPlayerNameTxt = (TextBox)PLayerDetailView.FindControl("editPlayerNameTextBox");
+        TextBox newJerseyNumberTxt = (TextBox)PLayerDetailView.FindControl("editJerseyNumberTextBox");
+        TextBox newDateOfBirthTxt = (TextBox)PLayerDetailView.FindControl("editDateOfBirthTextBox");
+
+        string newPlayerName = newPlayerNameTxt.Text;
+        string newJerseyNumber = newJerseyNumberTxt.Text;
+        string newDateOfBirth = newDateOfBirthTxt.Text;
+        conn.ConnectionString = connectionString;
+        SqlCommand com;
+        com = new SqlCommand("update players set playername=@newname, dateofbirth=@newDateOfBirth, jerseynumber=@newJerseyNumber where playerid=@playerid", conn);
+        com.Parameters.AddWithValue("@playerid", playerid);
+        com.Parameters.AddWithValue("@newname", newPlayerName);
+        com.Parameters.AddWithValue("@newDateOfBirth", newDateOfBirth);
+        com.Parameters.AddWithValue("@newJerseyNumber", newJerseyNumber);
+        flag = 0;
+        try
+        {
+            conn.Open();
+            com.ExecuteNonQuery();
+        }
+
+        finally
+        {
+            conn.Close();
+        }
+        PLayerDetailView.ChangeMode(DetailsViewMode.ReadOnly);
+        
+       databind();
+        bindDetail();
+    }
+
+    protected void PlayerGrid_RowEditing(object sender, GridViewEditEventArgs e)
+    {
+
+    }
+
+    protected void PLayerDetailView_ModeChanging(object sender, DetailsViewModeEventArgs e)
+    {
+        PLayerDetailView.ChangeMode(e.NewMode);        
+        bindDetail();
+    }
+    protected void Page_PreInit(object sender, EventArgs e)
+    {
+        if (Session["theme"] != null)
+        {
+            Page.Theme = Session["theme"].ToString();
+        }
+    }
+
+    protected void ProfileDetailView_ItemCommand(object sender, DetailsViewCommandEventArgs e)
+    {
+
+    }
+
+   
+    protected void ProfileDetailView_ItemUpdating(object sender, DetailsViewUpdateEventArgs e)
+    {
+
+        int playerid = (int)ProfieDetailsView.DataKey.Value;
+       // TextBox newPlayerProfileImage = (TextBox)ProfieDetailsView.FindControl("editProfileImage");
+        FileUpload newProfileImage = (FileUpload)ProfieDetailsView.FindControl("profileFileUpload");
+        Literal selectImageLiteral = (ProfieDetailsView.FindControl("selectImageLiteral") as Literal);
+
+        if (newProfileImage.HasFile)
+        {
+
+            Label newlabel = (Label)ProfieDetailsView.FindControl("selectImageLabel");
+            string fileName = newProfileImage.FileName;
+            newProfileImage.SaveAs(MapPath("~/IMAGES/Players/" + fileName));
+            string newImgPath = "~/IMAGES/Players/" + fileName;
+            string fileExtension = Path.GetExtension(fileName);
+
+            if (fileExtension.Equals(".jpg") || fileExtension.Equals(".jpeg") || fileExtension.Equals(".gif") || fileExtension.Equals(".png"))
+            {
+                if (fileName.Length < 100)
+                {
+                    try
+                    {
+                        conn.ConnectionString = connectionString;
+                        SqlCommand com;
+                        com = new SqlCommand("update players set profileimage=@newProfileImage where playerid=@playerid", conn);
+                        com.Parameters.AddWithValue("@playerid", playerid);
+                        com.Parameters.AddWithValue("@newProfileImage", newImgPath);
+                        try
+                        {
+                            conn.Open();
+                            com.ExecuteNonQuery();
+                        }
+
+                        finally
+                        {
+                            conn.Close();
+                        }
+                        ProfieDetailsView.ChangeMode(DetailsViewMode.ReadOnly);
+                        bindProfileDetail();
+                        bindDetail();
+                        databind();  
+                    }
+
+                    catch(Exception ex)
+                    {
+                        newlabel.Visible = true;
+                        newlabel.Text = "Upload status: The file could not be uploaded due to error :" + ex.Message;
+                    }
+                }
+                else
+                {
+                    newlabel.Visible = true;
+                    newlabel.Text = "Upload status: The file could not be uploaded.File Name too large";
+                }
+
+            }
+            else
+            {
+                newlabel.Visible = true;
+                newlabel.Text = "Please Select a .gif, .jpg or .png file!";
+
+            }
+        }
+        else
+        {
+            Label newlabel = (Label)ProfieDetailsView.FindControl("selectImageLabel");
+            newlabel.Visible = true;
+            newlabel.Text = "No file uploaded!";
+        }
+
+    }
+
+    protected void ProfileDetailView_ModeChanging(object sender, DetailsViewModeEventArgs e)
+    {
+        ProfieDetailsView.ChangeMode(e.NewMode);
+        bindProfileDetail();
+    }
+
+    protected void ProfieDetailsView_PageIndexChanging(object sender, DetailsViewPageEventArgs e)
+    {
+    }
+
+    protected void uploadButton_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void PLayerDetailView_ItemDeleting1(object sender, DetailsViewDeleteEventArgs e)
+    {
+
+        int selectedIndexRow;
+        selectedIndexRow = PlayerGrid.SelectedIndex;
+        int playerId = (int)PlayerGrid.DataKeys[selectedIndexRow].Value;
+
+        conn.ConnectionString = connectionString;
+        SqlCommand comDeletePlayer = new SqlCommand("delete from players where playerid = @id", conn);
+        comDeletePlayer.Parameters.AddWithValue("@id", playerId);
+
+        try
+        {
+            conn.Open();
+            comDeletePlayer.ExecuteNonQuery();
+            flag = 2;
+            
+            //Response.Redirect("ClubDetail.aspx");
+            Response.Redirect(Request.RawUrl);
+
+        }
+        catch (Exception ex)
+        {
+            throw (ex);
+        }
+        finally
+        {
+            comDeletePlayer.Dispose();
+            conn.Close();
+        }
+
+    }
+}
+//Name: Sidharth Choudhary    Student#- 300990285
